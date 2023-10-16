6e98f71d1305e8e6f8508a80d6261f3bbcbdf604
Bruno Cardoso <bruno@liveonsolutions.com>
2023-10-16 15:13:13 -0300

    feat: start a tailwind project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
