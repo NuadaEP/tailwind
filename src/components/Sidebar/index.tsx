06e3b3d0e03635310a9251bbeda1de7abf71477f
Bruno Cardoso <bruno@liveonsolutions.com>
2023-11-08 10:10:54 -0300

    sidebar for mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
+'use client';
 import React from 'react';
-import { Cog, LifeBuoy, Search } from 'lucide-react';
+import { Cog, LifeBuoy, Menu, Search } from 'lucide-react';
+import * as Collapsible from '@radix-ui/react-collapsible';
 
 import { Logo } from './Logo';
 import { MainNavigation } from './MainNavigation';
@@ -7,33 +9,46 @@ import { NavItem } from './MainNavigation/NavItem';
 import { UsedSpaceWidget } from './UsedSpaceWidget';
 import { Profile } from './Profile';
 import * as Input from '../Input';
+import { Button } from '../Button';
 
 const Sidebar = () => (
-  <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
-    <Logo />
+  <Collapsible.Root className="data-[state=open]:bottom-0 lg:data-[state=closed]:bottom-0 flex flex-col gap-6 border-b border-zinc-200 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
+    <Collapsible.Content
+      forceMount
+      className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+    >
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger className="lg:hidden" asChild>
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
+      </div>
 
-    <Input.Root>
-      <Input.Prefix>
-        <Search className="h-5 w-5 text-zinc-500" />
-      </Input.Prefix>
-      <Input.Control placeholder="Search" />
-    </Input.Root>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
-    <MainNavigation />
+      <MainNavigation />
 
-    <div className="mt-auto flex flex-col gap-6">
-      <nav className="space-y-0 5">
-        <NavItem title="Support" icon={LifeBuoy} />
-        <NavItem title="Settings" icon={Cog} />
-      </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0 5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
 
-      <UsedSpaceWidget />
+        <UsedSpaceWidget />
 
-      <div className="h-px bg-zinc-200" />
+        <div className="h-px bg-zinc-200" />
 
-      <Profile />
-    </div>
-  </aside>
+        <Profile />
+      </div>
+    </Collapsible.Content>
+  </Collapsible.Root>
 );
 
 export default Sidebar;
